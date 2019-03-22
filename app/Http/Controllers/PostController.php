1daf429447fe7a6c7a67abb28e50039e1f7155ca
Viral Jetani <viral.jetani@gmail.com>
2019-03-22 23:58:48 +1100

    charts updated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 use Excel;
 use App\Imports\PostsImport;
 use App\Charts\ReportChart;
-use App\Models\User;
 use App\District;
+use App\PostType;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -37,17 +38,110 @@ class PostController extends Controller
 
         $districts = District::withCount(['posts'])->get()->toArray();
         //dd($districts);
-        $data = collect([]);
+        $data1 = collect([]);
         foreach($districts as $key => $district){
             
-            $data->put($district['name'] , $district['posts_count']);
+            $data1->put($district['name'] , $district['posts_count']);
         }
         //dd($data->values());
         $chart = new ReportChart;
-        $chart->labels($data->keys());
-        $chart->dataset('Total Incidents in a District', 'bar', $data->values());
+        $chart->labels($data1->keys());
+        $chart->dataset('Total Incidents in a District', 'bar', $data1->values())->options(['backgroundColor' => [
+            'rgba(255, 99, 132, 0.9)',
+            'rgba(54, 162, 235, 0.9)',
+            'rgba(255, 206, 86, 0.9)',
+            'rgba(75, 192, 192, 0.9)',
+            'rgba(153, 102, 255, 0.9)',
+            'rgba(255, 99, 132, 0.9)',
+            'rgba(54, 162, 235, 0.9)',
+            'rgba(255, 206, 86, 0.9)',
+            'rgba(75, 192, 192, 0.9)',
+            'rgba(153, 102, 255, 0.9)',
+            'rgba(255, 159, 64, 0.9)'
+        ],
+        'borderColor' => [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ]]);
+        //dd($chart);
+        $incidentByType = PostType::withCount(['posts'])->get()->toArray();
+        //dd($incidentByType);
+        $data2 = collect([]);
+        foreach($incidentByType as $key => $incidentType){
+            
+            $data2->put($incidentType['name'] , $incidentType['posts_count']);
+        }
+        //dd($data->values());
+        $chart2 = new ReportChart;
+        $chart2->labels($data2->keys());
+        $chart2->dataset('Total Incidents by Incident Types', 'horizontalBar', $data2->values())->options(['backgroundColor' => [
+            'rgba(255, 99, 132, 0.9)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        'borderColor' => [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ]]);
+       
+        // Pie Chart
+        $victims = Post::select('victims', DB::raw('count(*) as posts_count'))
+        ->groupBy('victims')
+        ->get()->toArray();;
+        //dd($victims);
+        $data3 = collect([]);
+        foreach($victims as $key => $victim){
+            
+            $data3->put($victim['victims'] , $victim['posts_count']);
+        }
+        //dd($data3->values());
+        $chart3 = new ReportChart;
+        $chart3->labels($data3->keys());
+        $chart3->dataset('Total % of Incidents by Incident Types', 'pie', $data3->values())->options(['backgroundColor' => [
+            'rgba(255, 99, 132, 0.9)',
+            'rgba(54, 162, 235, 0.9)',
+            'rgba(255, 206, 86, 0.9)',
+            'rgba(75, 192, 192, 0.9)',
+            'rgba(153, 102, 255, 0.9)',
+            'rgba(255, 99, 132, 0.9)',
+            'rgba(54, 162, 235, 0.9)',
+            'rgba(255, 206, 86, 0.9)',
+            'rgba(75, 192, 192, 0.9)',
+            'rgba(153, 102, 255, 0.9)',
+            'rgba(255, 159, 64, 0.9)'
+        ],
+        'borderColor' => [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ]]);
+        $chart3->title("Total % of Victim Types");
 
-        return view('posts.reports',compact('chart'));
+        return view('posts.reports',compact('chart','chart2','chart3'));
 
         
     }
--- resources/views/posts/reports.blade.php
+++ resources/views/posts/reports.blade.php
@@ -9,22 +9,40 @@
 @endsection --}}
 
 @section('content')
-
+            <div class="col-md-10 offset-md-1 col-xs-10 offset-xs-1">
+                <br><h1>Incident Reports</h1>
+            </div>
 			<div class="col-md-10 offset-md-1 col-xs-10 offset-xs-1">
 
                 <div id="app">
                     {!! $chart->container() !!}
                 </div>
-                
+                <br><br><br>
+                <div id="app2">
+                    {!! $chart2->container() !!}
+                </div>
+                <br><br><br>
+                <div id="app3">
+                    {!! $chart3->container() !!}
+                </div>
+            </div>
                 <script>
                     var app = new Vue({
                         el: '#app',
                     });
+                    var app2 = new Vue({
+                        el: '#app2',
+                    });
+                    var app3 = new Vue({
+                        el: '#app3',
+                    });
                 </script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
                 {!! $chart->script() !!}
+                {!! $chart2->script() !!}
+                {!! $chart3->script() !!}
                 
-            </div>
+           
             
 
 @endsection
