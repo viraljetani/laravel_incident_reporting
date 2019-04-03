7eaea0854d2c5d2aca4ba865fd24086c3fe539ae
Viral Jetani <viral@enhancetv.com.au>
2019-04-03 19:57:05 +1100

    map pointer data updated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -339,7 +339,7 @@ class PostController extends Controller
                 $latlong = $geocoder->getCoordinatesForAddress($post->location);
                 $cords[$key][] = $latlong["lat"];
                 $cords[$key][] = $latlong["lng"];
-                $cords[$key][] = "<h5>$post->details</h5><p>$post->location</p>";
+                $cords[$key][] = "<h5>" . $post->postType->name ."</h5><p>$post->location</p>";
             }
         }
         return json_encode($cords);
