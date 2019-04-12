a138b7ec838c2a273da97ac98211be0ebc35643c
Viral Jetani <viral@enhancetv.com.au>
2019-04-13 08:34:28 +1000

    updated sidebar link

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -41,37 +41,41 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Analytics
+        Reports
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item @if(substr_count($currentRoute, 'reports')) active @endif">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-chart-pie"></i>
-        <span class="@if(substr_count($currentRoute, 'reports')) text-warning @endif">Reports</span>
-        </a>
-        <div id="collapseUtilities" class="collapse show" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Report Types:</h6>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.incident.by.district')) active @endif" href="{{ route('reports.incident.by.district') }}">Incidents by Districts</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.incident.by.type')) active @endif" href="{{ route('reports.incident.by.type') }}">Total Incidents by Type</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.incident.victims')) active @endif" href="{{ route('reports.incident.victims') }}">Victims of Incidents</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.victims.by.gender')) active @endif" href="{{ route('reports.victims.by.gender') }}">Victims By Gender</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.perpetrators.of.incidents')) active @endif" href="{{ route('reports.perpetrators.of.incidents') }}">Perpetrators Of Incidents</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.perpetrators.by.gender')) active @endif" href="{{ route('reports.perpetrators.by.gender') }}">Perpetrators By Gender</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.impact.by.incidents')) active @endif" href="{{ route('reports.impact.by.incidents') }}">Impact of Incidents</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.incidents.over.days')) active @endif" href="{{ route('reports.incidents.over.days') }}">Incidents Over Days</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.incidents.location')) active @endif" href="{{ route('reports.incidents.location') }}">Location of Incidents</a>
-            <a class="collapse-item @if(substr_count($currentRoute, 'reports.responses.taken')) active @endif" href="{{ route('reports.responses.taken') }}">Responses Taken</a>
-            
-            {{-- <a class="collapse-item" href="{{ route('reports') }}">Charts</a> --}}
-        </div>
-        </div>
+    <li class="nav-item">
+        <a class="nav-link less-padding @if(substr_count($currentRoute, 'reports.incident.by.district')) text-warning @endif" href="{{ route('reports.incident.by.district') }}"><span>Incidents by Districts</span></a>
+        
     </li>
-
-    
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.incident.by.type')) text-warning @endif" href="{{ route('reports.incident.by.type') }}"><span>Total Incidents by Type</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.incident.victims')) text-warning @endif" href="{{ route('reports.incident.victims') }}"><span>Victims of Incidents</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.victims.by.gender')) text-warning @endif" href="{{ route('reports.victims.by.gender') }}"><span>Victims By Gender</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.perpetrators.of.incidents')) text-warning @endif" href="{{ route('reports.perpetrators.of.incidents') }}"><span>Perpetrators Of Incidents</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.perpetrators.by.gender')) text-warning @endif" href="{{ route('reports.perpetrators.by.gender') }}"><span>Perpetrators By Gender</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.impact.by.incidents')) text-warning @endif" href="{{ route('reports.impact.by.incidents') }}"><span>Impact of Incidents</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.incidents.over.days')) text-warning @endif" href="{{ route('reports.incidents.over.days') }}"><span>Incidents Over Days</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.incidents.location')) text-warning @endif" href="{{ route('reports.incidents.location') }}"><span>Location of Incidents</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link less-padding  @if(substr_count($currentRoute, 'reports.responses.taken')) text-warning @endif" href="{{ route('reports.responses.taken') }}"><span>Responses Taken</span></a>
+    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
@@ -83,3 +87,8 @@
 
     </ul>
     <!-- End of Sidebar -->
+    <style>
+        .less-padding {
+            padding:0.1rem 1rem !important;
+        }
+    </style>
