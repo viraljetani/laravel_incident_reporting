07a7fedf2b0eb688a9373939e0b85dfbd820be33
Viral Jetani <viral@enhancetv.com.au>
2019-04-13 09:51:25 +1000

    map zoomed out

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,7 +128,7 @@
         }),
         latlng = L.latLng(-13.9626, 33.7741);
 
-    var map = L.map('map-canvas', {center: latlng, zoom: 9, layers: [tiles]});
+    var map = L.map('map-canvas', {center: latlng, zoom: 8, layers: [tiles]});
     var points = function () {
         var tmp = null;
         $.ajax({
